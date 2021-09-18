2e92ea24ade39710592ba8014336d6e17f108ac2
shahanneda <shahan.neda@gmail.com>
2021-09-18 00:56:34 -0400

    created basic repo and project structure
    Co-authored-by: Khloe Ramdhan <khloe-r@users.noreply.github.com>

---
+++ src/components/logout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Router, Link } from "@reach/router"
+import { Button } from "react-bootstrap";
+
+export default function LogOut() {
+	return (
+		<div>
+            <Button variant="primary">Logo</Button> 
+		</div>
+	)
+}
